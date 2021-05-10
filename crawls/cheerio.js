b4ef544714a3a11c5f071797fa7fbab894fead80
Dang Le Minh Truong <truongdlm@fpt.com.vn>
2021-05-10 15:53:12 +0700

    peft crawl

--- crawls/cheerio.js
+++ crawls/cheerio.js
@@ -5,43 +5,99 @@ const axios = require('axios');
 const run = async (keyword) => {
   try {
     const m3u8Urls = []
-    if(!hasNumber(keyword)) keyword = keyword + 'Tập 1'
 
-    function hasNumber(myString) {
-      return /\d/.test(myString);
+    keyword = 'Conan tap'
+
+    const keywordRemoveAccents = removeAccents(keyword)
+    const isMovieInKeyword = keywordRemoveAccents.indexOf('tap') !== -1 ? false : true
+    console.log(keywordRemoveAccents);
+    let episode = ''
+    if(!isMovieInKeyword) {
+      keyword = keywordRemoveAccents.split('tap')[0]
+      episode = keywordRemoveAccents.split('tap')[1].trim()
+      console.log(episode);
+      episode = episode === '' ? 1 : episode
     }
-    const googleResult = await got(`https://www.google.com/search?q=Phim ${keyword}`);
-    const $ = cheerio.load(googleResult.body);
-    const urls = []
-    $("div a").each(function() {
-      var link = $(this);
-      var text = link.attr('href');
+    const phePhimRes = await got(`https://phephimz.net/tim-kiem?q=${keyword}`);
+    const $ = cheerio.load(phePhimRes.body);
+    const videos = []
+    $("#slide-episodes").find('> div.col-xs-6').map((i, element) => {
+      const url = $(element).find('a.film-title').attr('href')
+      const title = $(element).find('a.film-title').text()
+      const subtext = $(element).find('div.film-tag .sub').text()
+      const isMovie = removeAccents(subtext).indexOf('tap') !== -1 ? false : true
+      let thumbnail = $(element)
+      .find('a.film-cover > .poster')
+      .css('background-image');
 
-      urls.push(text.split('/url?q=')[1])
-    });
-    const urPhePhim = urls.filter(item => item !== undefined && new URL(item).origin.includes('phephimz.net'))[0]
-    console.log(urPhePhim);
-		const urlDetail = await got(urPhePhim.split('&')[0]);
-		const idVideo = String(urlDetail.body).split('var EpisodeID = ')[1].split(' ')[0].match(/\d+/)[0]
-    console.log(idVideo)
-    const m3u8Url = await initPlayer(`https://phephim.xyz/embed/video?id=${idVideo}`)
-    console.log(m3u8Url);
-    m3u8Urls.push(m3u8Url)
+      if (thumbnail) {
+        thumbnail = thumbnail.replace(/^url\(["']?/, '').replace(/["']?\)$/, '')
+      }
+      videos.push({
+        url,
+        title,
+        isMovie,
+        thumbnail
+      })
+    })
+
+
+    if(!isMovieInKeyword) {
+      console.log('series');
+      const videoSeries = [...videos].filter(video => !video.isMovie)
+      await Promise.all(
+        videoSeries.map(async (video) => {
+          const m3u8Url = await getM3u8Url(video, episode)
+          m3u8Url && m3u8Urls.push({
+            title: video.title,
+            url: m3u8Url,
+            thumbnail: video.thumbnail
+          })
+        })
+      )
+    }
+
+    // get movie
+    if(isMovieInKeyword) {
+      console.log('movies');
+      const videoMovies = [...videos].filter(video => video.isMovie)
+      await Promise.all(
+        videoMovies.map(async (video) => {
+          if(!video.isMovie) return
+          const m3u8Url = await getM3u8Url(video)
+          m3u8Url && m3u8Urls.push({
+            title: video.title,
+            url: m3u8Url,
+            thumbnail: video.thumbnail
+          })
+        })
+      )
+    }
+    console.log(m3u8Urls);
     return m3u8Urls
-    // const $ = cheerio.load(urlDetail.body);
+    
 	} catch (error) {
 		console.log(error);
     return []
-		//=> 'Internal server error ...'
 	}
 }
+
+const getM3u8Url = async (video, episode) => {
+  const prefixEndPoint = episode  ? `tap-${episode}.html` : 'xem-phim.html' 
+  console.log(prefixEndPoint);
+  const urlDetail = await got(`${video.url}/${prefixEndPoint}`);
+  const idVideo = Number(urlDetail.body.match(/var EpisodeID = (.*);/)[1])
+  console.log(idVideo);
+  return await initPlayer(`https://phephim.xyz/embed/video?id=${idVideo}`)
+}
+
 async function initPlayer(url) {
   const { protocol, host, search } = new URL(url)
   if (!/id=(\d+)/.test(search)) return handleShowError(0, 'Không tìm thấy id của video.')
   vid = search.match(/id=(\d+)/)[1]
   const vinfo = protocol + '//' + host + '/api/getvinfo?callback=json&vid=' + vid
   const video = await fetchApi(vinfo)
-  if(!video) return
+  if(!video || video?.code !==0) return
   const m3u8Url = video.data.host + '/vod/v2/packaged:mp4/' + video.data.mid + '/playlist.m3u8'
 
   return m3u8Url
@@ -62,7 +118,15 @@ const fetchApi = async (url) => {
   }
 }
 
+const hasNumber = (myString) => {
+  return /\d/.test(myString);
+}
 
+const removeAccents = (str) => {
+  return str.normalize('NFD')
+            .replace(/[\u0300-\u036f]/g, '')
+            .replace(/đ/g, 'd').replace(/Đ/g, 'D').toLocaleLowerCase();
+}
 module.exports = {
   run: run
 }
